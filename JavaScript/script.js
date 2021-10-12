4c715c2878305e59900ca8dd098cf70088b8f060
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2021-10-12 18:12:07 +0200

    added working create new task document element

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -9,43 +9,60 @@ const displayedComplitedTaskNr = document.querySelector('header > h3 > span');
 
 console.log(newTaskInput.value, taskSection, displayedComplitedTaskNr.innerText);
 
-/* 
-    add eventlistener to input-button that creates a new task with input-value as innertext on click.
+ 
+// add eventlistener to input-button that creates a new task with input-value as innertext on click.
 
-    display taskDone as innerText to displayedComplitedTaskNr
-        - needs to be updated when a task is changed
+// display taskDone as innerText to displayedComplitedTaskNr
+//     - needs to be updated when a task is changed
 
-    create task obj/class.
-        create variable holding an unik id for each task.
-            - global const id = 0 => id++ when new task is created
+// create task obj/class.
+//     create variable holding an unik id for each task.
+//         - global const id = 0 => id++ when new task is created
 
-        create variable holding task(todo) text from input.value (taskInnerText?)
-        
-        create variable holding if task is completed or not  (isTaskCompleted?)
-        - true or false value
-        - needs to ++/-- taskDone (function?)
-        
-        create delete task method from dom and taskStorage
-            - remove parenet(todo-container) from DOM/display
-        
-        create method that controls CSS style for completed task or not.
-            - set class .completed depending on isTaskCompleded value
+//     create variable holding task(todo) text from input.value (taskInnerText?)
     
-        create a display method that pushes this task to the dom.
-            (can i do this on input-button click instead?)
-
+//     create variable holding if task is completed or not  (isTaskCompleted?)
+//     - true or false value
+//     - needs to ++/-- taskDone (function?)
     
-    create a function that appends new task to taskSection
-        - needs functionaliy that appends elements
-            - li
-            - p
-            - button
-            - span(for animation)?
-
-    create a function that saves to array 
-        - push new task obj to taskStorage[]
+//     create delete task method from dom and taskStorage
+//         - remove parenet(todo-container) from DOM/display
     
-    create a function that remove an task from taskStorge when not displayed in the dom.
-        - filter out the task from TaskStorage ande save the new values to taskStorage
-          || splice out the selected task.
-*/
+//     create method that controls CSS style for completed task or not.
+//         - set class .completed depending on isTaskCompleded value
+
+//     create a display method that pushes this task to the dom.
+//         (can i do this on input-button click instead?)
+
+// create a function that appends new task to taskSection
+//     - needs functionaliy that appends elements
+//         - li
+//         - p
+//         - button
+//         - span(for animation)?
+
+function newElement(taskId, taskText){
+    let element = document.createElement('li');
+        taskId !== null||undefined? element.setAttribute('id', taskId) : element.removeAttribute('id');
+        element.classList.add('todo-container')
+        
+        const text =  document.createElement('p');
+            text.innerText = taskText;
+            element.appendChild(text);
+
+        const deleteBtn  =  document.createElement('button');
+            deleteBtn.innerText = '\uD83D\uDDD1\uFE0F';
+            element.appendChild(deleteBtn);
+    
+    return element;
+}
+
+taskSection.appendChild(newElement(80, 'Learn javscript'))
+
+// create a function that saves to array 
+//     - push new task obj to taskStorage[]
+
+// create a function that remove an task from taskStorge when not displayed in the dom.
+//     - filter out the task from TaskStorage ande save the new values to taskStorage
+//         || splice out the selected task.
+
