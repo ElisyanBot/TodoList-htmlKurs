d05a761abf11032853731dd0d61823ab9aee20bd
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2021-10-12 19:35:01 +0200

    working <span> for 0 completed

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,57 +1,35 @@
 const todoStorage = []
-let taskDone = 0;
-let id = 0;
+let tasksComplete = 0;
+let taskIdNumber = 0;
 
 //fetched dynamic sections in the html
 const newTaskInput = document.querySelector('#newTodoInput-container > input');
 const newTaskBtn = document.querySelector('#newTodoInput-container > button');
 const taskSection = document.querySelector('#todos-section');
-const displayedComplitedTaskNr = document.querySelector('header > h3 > span');
+const displayedCompletedTaskNr = document.querySelector('header > h3 > span');
 
-console.log(newTaskInput.value, taskSection, displayedComplitedTaskNr.innerText);
-
+function changeTasksCompleteNr(value){
+    if(value === true) { 
+        tasksComplete++;
+    } else {
+        tasksComplete >= 1 ? tasksComplete-- : tasksComplete = 0;
+    }
+    displayedCompletedTaskNr.innerText = tasksComplete;
+}
 
 // add eventlistener to input-button that creates a new task with input-value as innertext on click.
  
 newTaskBtn.addEventListener('click', ()=>{
-    todoStorage.push(new task(id, newTaskInput.value));
+    todoStorage.push(new task(taskIdNumber, newTaskInput.value));
     let i = todoStorage.length - 1;
     
     todoStorage[i].createTask();
-    id++;
+    taskIdNumber++;
 })
-
-// display taskDone as innerText to displayedComplitedTaskNr
-//     - needs to be updated when a task is changed
-
-// create task obj/class.
-//     create variable holding an unik id for each task.
-//         - global const id = 0 => id++ when new task is created
-
-//     create variable holding task(todo) text from input.value (taskInnerText?)
-    
-//     create variable holding if task is completed or not  (isTaskCompleted?)
-//     - true or false value
-//     - needs to ++/-- taskDone (function?)
-    
-//     create delete task method from dom and taskStorage
-//         - remove parenet(todo-container) from DOM/display
-    
-//     create method that controls CSS style for completed task or not.
-//         - set class .completed depending on isTaskCompleded value
-
-//     create a display method that pushes this task to the dom.
-//         - li
-//         - p
-//         - button
-//         - span(for animation)?
-
-
-
 
 class task {
     constructor(id, innerText){
-        this.id = 'id' + id;
+        this.id = 'id' + id; // don't remove 'id:', it makes the querySelector work.
         this.innerText = innerText;
         this.isCompleted = false;
     }
@@ -80,9 +58,11 @@ class task {
         if(this.isCompleted === false){
             this.isCompleted = true
             taskText.classList.add('completed')
+            changeTasksCompleteNr(this.isCompleted)
         } else {
             this.isCompleted = false
             taskText.classList.remove('completed')
+            changeTasksCompleteNr(this.isCompleted)
         }
     }
 
